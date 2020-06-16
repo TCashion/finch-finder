a7ad2c6f054042f25d329b1aafc500981e0bd940
TCashion <58757351+TCashion@users.noreply.github.com>
2020-06-16 11:25:32 -0600

    stub up templates and styling

--- main_app/views.py
+++ main_app/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 # Create your views here.
 def home(request):
-    return HttpResponse('<h1>home</h1>')
+    return render(request, 'main_app/home.html')
