94b69e0941c6c6105dc44b68cb3366bc9478bf22
TCashion <58757351+TCashion@users.noreply.github.com>
2020-06-17 10:06:12 -0600

    change nav url to template tag

--- main_app/urls.py
+++ main_app/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home'), 
+    path('', views.home, name='home'),
     path('birds/', views.birds_index, name='index'),
     path('birds/<int:bird_id>', views.birds_detail, name='detail'),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
 from .models import Bird
 
+
 # Create your views here.
 def home(request):
     return render(request, 'main_app/home.html')
+
 
 def birds_index(request):
     birds = Bird.objects.all()
