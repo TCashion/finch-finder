433946572c23698e52f4508d3fc33606ce212f6a
TCashion <58757351+TCashion@users.noreply.github.com>
2020-06-17 11:47:04 -0600

    update birds_detail namespace

--- main_app/urls.py
+++ main_app/urls.py
@@ -4,5 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('birds/', views.birds_index, name='birds_index'),
-    path('birds/<int:bird_id>/', views.birds_detail, name='detail'),
+    path('birds/<int:bird_id>/', views.birds_detail, name='birds_detail'),
 ]
