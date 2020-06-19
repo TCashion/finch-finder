9558ff5923c12ef41dc407645d33e5504e1f377a
TCashion <58757351+TCashion@users.noreply.github.com>
2020-06-19 12:40:24 -0600

    update location index photo cards

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
-from .models import Bird, Sighting
+from .models import Bird, Sighting, Location, BirdPhoto, LocationPhoto
 
 # Register your models here.
 admin.site.register(Bird)
 admin.site.register(Sighting)
+admin.site.register(Location)
+admin.site.register(BirdPhoto)
+admin.site.register(LocationPhoto)
